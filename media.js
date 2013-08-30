0befcdb33b152e53cb41f890af014b97319c5c70
didiyeah <didierlovera@gmail.com>
2013-08-30 15:07:33 +0200

    Update media.js
    #main

--- media.js
+++ media.js
@@ -5,10 +5,10 @@ function ajuste() {
 	var h = $(window).height();
 	
 	//
-	// point de rupture du design : w<540px
+	// point de rupture du design : w<740px
 	//
 	
-	if(w>540){ // scroll horizontal
+	if(w>740){ // scroll horizontal
 
 		// calcul largeur et hauteur en pixel pour le main 
 		var n_section = $("section").length; // nombre de sections (colonne)
@@ -19,9 +19,9 @@ function ajuste() {
 		$("section").css({"display":"inline-block","height":"100%", "margin-bottom":"20px"});
 		$("header").css({"position":"fixed"});
 		$("footer").css({"position":"fixed"});
-		$("main").css({"margin-top":"100px","width":larg_main, "height":haut_main});
+		$("#main").css({"margin-top":"100px","width":larg_main, "height":haut_main});
 
-	} else{ // w<540px -> scroll vertical
+	} else{ // w<740px -> scroll vertical
 		
 		// calcul largeur et hauteur en pixel pour le main 
 		var larg_main = w-60;
@@ -31,10 +31,13 @@ function ajuste() {
 		$("section").css({"display":"block","height":"400px"});
 		$("header").css({"position":"relative"});
 		$("footer").css({"position":"relative"});
-		$("main").css({"margin-top":"20px","width":larg_main, "height":haut_main});
+		$("#main").css({"margin-top":"20px","width":larg_main, "height":haut_main});
 	}
 	
 	$("header").html("fenetre : largeur="+w+" hauteur="+h+" - screen : largeur="+screen.availWidth+" hauteur="+screen.availHeight+" - main : largeur="+larg_main+" hauteur="+haut_main)
 				.css("color","#fff");
 	console.log(w+" - "+h+" largeur du main ="+larg_main);
 }
+
+$(window).resize(function () {ajuste();});
+$(document).ready(function () {ajuste();});
